19ecdb25b88e00f3614a9acc1549e332066417d9
Mark <meichangliang@outlook.com>
2019-02-24 17:22:33 +0800

    add

--- next_practice/components/layout/index.js
+++ next_practice/components/layout/index.js
@@ -15,11 +15,20 @@ const Layout = class extends React.Component {
       <div>
         <Head>
           <title>{title}</title>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
+          {/* <meta charset="utf-8" /> 这个默认会存在 */}
+          <meta name="renderer" content="webkit" />
+          <meta http-equiv="Cache-Control" content="no-siteapp" />
+          <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+          <meta name="viewport" content="width=width=device-width" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+          <meta name="format-detection" content="telephone=no, email=no" />
+          <meta name="keywords" content="个人的nextdemo,next,demo,react" />
+          <meta name="description" content="私人使用的工具集合" />
+          <meta name="author" content="Mark,670188307@qq.com" />
+          <meta name="robots" content="index,follow" />
+          <meta name="apple-mobile-web-app-title" content="next" />
+          <link rel="shortcut icon" href="../../static/image/favicon.ico" />
         </Head>
         <header>
           <nav>
